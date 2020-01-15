15f62d7ae7bdf85940b7e6b2fc360d5293fc3eaf
saayan <saayan8981@gmail.com>
2020-01-15 21:46:20 +0530

    crud application

---
+++ employee_project/employee_register/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .forms import *
+
+# Create your views here.
+
+def employee_list(request):
+    return render(request,"employee_register/employee_list.html")
+
+def employee_form(request):
+    form =EmployeeForm()
+    return render(request,"employee_register/employee_form.html",{"form":form}) 
+
+def employee_delete(request):
+    pass
