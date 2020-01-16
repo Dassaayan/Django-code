2480b62bdb869fc0fbe02c926a5b1fa4ee20d33b
Caavo <saayan8981@gmail.com>
2020-01-16 16:36:14 +0530

    #4 crud apps

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -3,21 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
 from .forms import *
+from .models import *
 
 # Create your views here.
 
 def employee_list(request):
-    return render(request,"employee_register/employee_list.html")
+    context= {'employee_list':Employee.objects.all()}
+    return render(request,"employee_register/employee_list.html",context)
 
 def employee_form(request):
-	if request.method=="GET":
-	    form =EmployeeForm()
-	    return render(request,"employee_register/employee_form.html",{"form":form})
-	else:
-	    form =EmployeeForm(request.POST)
-	    if form.is_valid():
-	        form.save()
-	    return redirect('/employee/list/')         
+    if request.method=="GET":
+        form =EmployeeForm()
+        return render(request,"employee_register/employee_form.html",{"form":form})
+    else:
+        form =EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list/')         
 
 def employee_delete(request):
     pass
