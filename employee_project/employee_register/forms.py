b1fb37a889189fb4f7c0cef83a2d081703684657
saayan <saayan8981@gmail.com>
2020-01-16 08:29:06 +0530

    #2 crud application

--- employee_project/employee_register/forms.py
+++ employee_project/employee_register/forms.py
@@ -3,6 +3,16 @@ from .models import *
 
 class EmployeeForm(forms.ModelForm):
 
-     class Meta:
-     	model=Employee
-     	fields='__all__'
+    class Meta:
+        model=Employee
+        fields='__all__'
+        labels={
+             "fullname": "Full Name",
+             "emp_code": "Emp. Code",
+             "mobile": "Mobile number(+91)",
+             "position": "Positions"
+        }
+
+    def __init__(self,*args,**kwargs):
+        super(EmployeeForm,self).__init__(*args,**kwargs)
+        self.fields["position"].empty_label="Select"
