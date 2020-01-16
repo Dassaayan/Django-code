a9e74a6f38e90010bad02259a79b867836786662
Caavo <saayan8981@gmail.com>
2020-01-16 17:41:52 +0530

    #5 crud apps

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-    url('form/',views.employee_form),
-    url('list/',views.employee_list)
+    url('form/',views.employee_form,name='employee_insert'),
+    url('(?P<id>\d+)/$',views.employee_form,name='employee_update'),
+    url('list/',views.employee_list,name='employee_list')
 ]
